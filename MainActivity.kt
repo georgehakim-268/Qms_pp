b312339395c5aa0fd470499b65c7d71a24801ff1
georgehakim-268 <georgefawzy2023@gmail.com>
2025-07-09 04:20:13 +0300

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,7 @@
+
+package com.example.qmsapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- main.dart
+++ main.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(title: Text('QMS App')),
-      body: Center(child: Text('مرحبًا بك في تطبيق الجودة')),
+      body: Center(child: Text('مرحبًا بك في تطبيق إدارة الجودة')),
     ),
   ));
 }
